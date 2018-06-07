bd5c5ed9827e22554bc4149bdbc24618f0c023b0
Vincent Feng <iVincentFeng@live.com>
2018-06-07 16:45:41 +0800

    Minor changes

--- src/main/kotlin/mapreduce/wordcount/WordCountMain.kt
+++ src/main/kotlin/mapreduce/wordcount/WordCountMain.kt
@@ -9,29 +9,34 @@ import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 
 
-fun main(args: Array<String>) {
-    val config = Configuration()
+class WordCountMain {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            val config = Configuration()
 
-    val job = Job.getInstance(config, "vifeng_wordcount")
-    job.setJarByClass(WordCountMapper::class.java)
+            val job = Job.getInstance(config, "vifeng_wordcount")
+            job.setJarByClass(WordCountMapper::class.java)
 
-    FileInputFormat.setInputPaths(job, Path(args[0]))
+            FileInputFormat.setInputPaths(job, Path(args[0]))
 
-    job.mapperClass = WordCountMapper::class.java
-    job.mapOutputKeyClass = Text::class.java
-    job.mapOutputValueClass = LongWritable::class.java
+            job.mapperClass = WordCountMapper::class.java
+            job.mapOutputKeyClass = Text::class.java
+            job.mapOutputValueClass = LongWritable::class.java
 
-    job.combinerClass = WordCountReducer::class.java
+            job.combinerClass = WordCountReducer::class.java
 
-    job.partitionerClass = WordCountPartitioner::class.java
-    job.numReduceTasks = 4
+            job.partitionerClass = WordCountPartitioner::class.java
+            job.numReduceTasks = 4
 
-    job.reducerClass = WordCountReducer::class.java
+            job.reducerClass = WordCountReducer::class.java
 
-    job.outputKeyClass = Text::class.java
-    job.outputValueClass = LongWritable::class.java
+            job.outputKeyClass = Text::class.java
+            job.outputValueClass = LongWritable::class.java
 
-    FileOutputFormat.setOutputPath(job, Path(args[1]))
+            FileOutputFormat.setOutputPath(job, Path(args[1]))
 
-    job.waitForCompletion(true)
+            job.waitForCompletion(true)
+        }
+    }
+
 }
